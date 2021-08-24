dd1ebfbfa6154ec69695ea4138b840c89608dd37
Aromal S <60032953+aromalsanthosh@users.noreply.github.com>
2021-08-24 15:56:17 +0000

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function App() {
+  return (
+    <div className="header">
+      <h1>Text to Emoji</h1>
+    </div>
+  );
+}
+
+export default App;
